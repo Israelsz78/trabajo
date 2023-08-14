e888d072adf728bc89783be25cdbfdc1c8017016
Israel Santamaria Zarate <israelz961@gmail.com>
2023-08-13 18:44:01 -0700

    Trabajo canva

---
+++ main.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 100, 100);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(90, 90, 100, 100);
+
+ctx.fillStyle = "red";
+ctx.fillRect(170, 170, 100, 100);
